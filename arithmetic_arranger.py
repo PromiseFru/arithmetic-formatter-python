c9435aa19b2f99bdd67ed993e3a604aa0f822663
Promise Fru <promisefru1@gmail.com>
2020-12-29 07:25:17 +0100

    updates made

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,22 @@
-def arithmetic_arranger(problems):
+import re
 
+def arithmetic_arranger(problems, solution = False):
+    # search each probelm for operators
+    for item in problems:
+        operator = re.search('[*|/]', item)
+        if operator:
+            print("Error: Operator must be '+' or '-'") 
+            return
 
-    return arranged_problems
+    # more than five problems
+    if len(problems) > 5:
+        print('Error: Too many problems') 
+        return
+
+    for item in problems:
+        operands = re.split('\+|\-', item)
+        print(operands)
+    # return arranged_problems
+
+test = ["32 + 698", "3801 - 2", "45 * 43", "123 + 49"]
+arithmetic_arranger(test)
