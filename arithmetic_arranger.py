ac108521f155a13ba92cbd84cf74176fec9a8d53
Promise Fru <promisefru1@gmail.com>
2020-12-30 03:25:50 +0100

    updates made

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,20 +1,28 @@
 import re
 
 def arithmetic_arranger(problems, solution = False):
-    # search each probelm for operators
-    for item in problems:
-        operator = re.search('[*|/]', item)
-        if operator:
-            print("Error: Operator must be '+' or '-'") 
-            return
-
     # more than five problems
     if len(problems) > 5:
         print('Error: Too many problems') 
         return
+        
+    # search each problem for operators
+    for item in problems:
+        operator = re.search('[*|/]', item)
+        operands = re.split('[+|-]', item)
+
+        if operator:
+            print("Error: Operator must be '+' or '-'") 
+            return
+        
+        for operand in operands:
+            stripOperand = operand.strip()
+            nonDigit = re.search('[^0-9]', stripOperand)
+            if nonDigit:
+                print('Error: Numbers must only contain digits')
+                return
 
     for item in problems:
-        operands = re.split('\+|\-', item)
         print(operands)
     # return arranged_problems
 
