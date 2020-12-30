fdcf78b22f032e871d5847c92bcde15dc3ad0210
Promise Fru <promisefru1@gmail.com>
2020-12-30 15:24:42 +0100

    updated returns

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -3,43 +3,62 @@ import re
 def arithmetic_arranger(problems, solution = False):
     # more than five problems
     if len(problems) > 5:
-        print('Error: Too many problems.') 
-        return
+        return 'Error: Too many problems.'
         
-    # search each problem for operators
+    arrangged_problems = ""
+
+    # search each problem for operators adn operands
     for item in problems:
         operator = re.search('[*|/]', item)
         operands = re.split('[+|-]', item)
 
         if operator:
-            print("Error: Operator must be '+' or '-'.") 
-            return
+            return "Error: Operator must be '+' or '-'."
         
         for operand in operands:
             stripOperand = operand.strip()
             nonDigit = re.search('[^0-9]', stripOperand)
             if nonDigit:
-                print('Error: Numbers must only contain digits.')
-                return
+                return 'Error: Numbers must only contain digits.'
             
             if len(stripOperand) > 4:
-                print('Error: Numbers cannot be more than four digits.')
-                return
+                return 'Error: Numbers cannot be more than four digits.'
 
+    # print top digits
+    for item in problems:
+        operands = re.split('[+|-]', item)
         first = operands[0].strip()
         second = operands[1].strip()
         sign = re.search('[+|-]', item).group()
         biggest = max([int(first), int(second)])
         dashes = ""
-
         for i in range(len(str(biggest))+2):
             dashes += "-"
 
-        arrangged_problems = "{}\n{} {}\n{}"
+        sol = "{}   "
 
-        print(arrangged_problems.format(first.rjust(len(str(biggest))+2), sign, second.rjust(len(str(biggest))), dashes))
-        # print(dashes)
-    # return arranged_problems
+        arrangged_problems += sol.format(first.rjust(len(str(biggest))+2))
+
+    # line break
+    arrangged_problems += "\n"
+
+    # print bottom digits
+    for item in problems:
+        operands = re.split('[+|-]', item)
+        first = operands[0].strip()
+        second = operands[1].strip()
+        sign = re.search('[+|-]', item).group()
+        biggest = max([int(first), int(second)])
+        dashes = ""
+        for i in range(len(str(biggest))+2):
+            dashes += "-"
+
+        sol = "{} {}  "
+
+        arrangged_problems += sol.format(sign, second.rjust(len(str(biggest))))
+        
+    print(arrangged_problems)
+#     # return test
 
 test = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
 arithmetic_arranger(test)
